ffaa05c21caa00183a5a29f45c254df68e3234b5
Wilmer Gaona <wgaonar@gmail.com>
2023-12-18 11:48:36 -0600

    Better avoidance obstacle reaction was added

--- main.ts
+++ main.ts
@@ -1,11 +1,19 @@
 maqueenPlusV2.I2CInit()
 basic.forever(function () {
-    if (input.lightLevel() > 25) {
-        basic.showIcon(IconNames.Yes)
-        maqueenPlusV2.controlMotor(maqueenPlusV2.MyEnumMotor.AllMotor, maqueenPlusV2.MyEnumDir.Forward, input.lightLevel())
+    if (maqueenPlusV2.readUltrasonic(DigitalPin.P13, DigitalPin.P14) > 20) {
+        if (input.lightLevel() > 25) {
+            basic.showIcon(IconNames.Yes)
+            maqueenPlusV2.controlMotor(maqueenPlusV2.MyEnumMotor.AllMotor, maqueenPlusV2.MyEnumDir.Forward, input.lightLevel())
+        } else {
+            basic.showIcon(IconNames.No)
+            maqueenPlusV2.controlMotor(maqueenPlusV2.MyEnumMotor.LeftMotor, maqueenPlusV2.MyEnumDir.Forward, 255)
+            maqueenPlusV2.controlMotor(maqueenPlusV2.MyEnumMotor.RightMotor, maqueenPlusV2.MyEnumDir.Forward, 50)
+        }
     } else {
-        basic.showIcon(IconNames.No)
-        maqueenPlusV2.controlMotor(maqueenPlusV2.MyEnumMotor.LeftMotor, maqueenPlusV2.MyEnumDir.Forward, 255)
-        maqueenPlusV2.controlMotor(maqueenPlusV2.MyEnumMotor.RightMotor, maqueenPlusV2.MyEnumDir.Forward, 50)
+        basic.showIcon(IconNames.Ghost)
+        maqueenPlusV2.controlMotor(maqueenPlusV2.MyEnumMotor.AllMotor, maqueenPlusV2.MyEnumDir.Backward, 50)
+        basic.pause(100)
+        maqueenPlusV2.controlMotor(maqueenPlusV2.MyEnumMotor.LeftMotor, maqueenPlusV2.MyEnumDir.Forward, 50)
+        maqueenPlusV2.controlMotor(maqueenPlusV2.MyEnumMotor.RightMotor, maqueenPlusV2.MyEnumDir.Backward, 50)
     }
 })
